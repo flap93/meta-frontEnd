d76eb9b25e38014e2a5f224dfaddd0858263a761
Felix Palacios <flpalacios93@gmail.com>
2022-08-21 15:28:25 -0400

    mini dom blurry image

--- OOP book list/app.js
+++ OOP book list/app.js
@@ -8,14 +8,40 @@ function Book(title, author, isbn) {
 // !UI constructor
 function UI() {}
 
-// !Add Book To List
+// !7 .Add Book To List
 UI.prototype.addBookToList = function (book) {
   const list = document.getElementById("book-list");
   // Create tr element
   const row = document.createElement("tr");
-  console.log(tr);
+  // Insert columns
+  row.innerHTML = `
+  <td>${book.title}</td>
+  <td>${book.author}</td>
+  <td>${book.isbn}</td>
+  <td><a href="#" class="delete">X<a></td>
+`;
+  list.appendChild(row);
 };
 
+//! 8. validation , show alert
+UI.prototype.showAlert = function (message, className) {
+  // create div
+  const div = document.createElement("div");
+  // Add classes
+  div.className = `alert ${className}`;
+  //add text
+  div.appendChild(document.createTextNode(message));
+  // get parent
+  const container = document.querySelector(".container");
+  // get form
+  const form = document.querySelector("#book-form");
+  // insert alert
+  container.insertBefore(div, form);
+  // time out after 3 seconds
+  setTimeout(function () {
+    document.querySelector(".alert").remove();
+  }, 3000);
+};
 //! 2. //
 document.getElementById("book-form").addEventListener("submit", function (e) {
   //! 3. // get form values
@@ -26,13 +52,26 @@ document.getElementById("book-form").addEventListener("submit", function (e) {
   //! 4. //  Instantiate a book object
   const book = new Book(title, author, isbn);
 
-  // !5. Instantiate a UI
+  // !5. Instantiate UI
 
   const ui = new UI();
 
-  // !6. Add book to list
+  // Validate
 
-  ui.addBookToList(book);
+  if (title === "" || author === "" || isbn === "") {
+    // Error ALERT
+    ui.showAlert("Please fill in all fields", "error");
+  } else {
+    // !6. Add book to list
+    ui.addBookToList(book);
+
+    // Show succes
+
+    ui.showAlert("Book added!", "succes");
+
+    // clear fields
+    ui.clearFields();
+  }
 
   e.preventDefault();
 });
