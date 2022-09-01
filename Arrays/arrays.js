26b21541f8a3c1b0b06bb8ee9555b207b3a33080
Felix Palacios <flpalacios93@gmail.com>
2022-08-31 23:46:47 -0400

    Add loop for objects

---
+++ Arrays/arrays.js
@@ -0,0 +1,5 @@
+//* you can loop throguh arrays like this :
+const colors = ["red", "orange", "yellow"];
+for (var color of colors) {
+  console.log(color);
+}
--- objetcs/objects.js
+++ objetcs/objects.js
@@ -10,3 +10,53 @@ console.log(myCar.make);
 myCar["make"] = "Ford";
 myCar["model"] = "Mustang";
 myCar["year"] = 1969;
+
+// !Build in methods for objects to access the keys of the object
+
+const car2 = {
+  speed: 200,
+  color: "red",
+};
+console.log(Object.keys(car2)); // ['speed','color']
+
+// !Build in methods for objects to access the values of the object
+
+const car3 = {
+  speed: 300,
+  color: "yellow",
+};
+console.log(Object.values(car3)); // [ 300, 'yellow' ]
+
+// !Another useful method  is the OBJECT.ENTRIES()
+//  *retuns an array of the keys and values of the object
+
+const car4 = {
+  speed: 400,
+  color: "magenta",
+};
+console.log(Object.entries(car4)); // [ ['speed', 400], ['color', 'magenta'] ]
+
+// HOW loop through an object :
+
+var clothingItem = {
+  price: 50,
+  color: "beige",
+  material: "cotton",
+  season: "autumn",
+};
+
+for (key of Object.keys(clothingItem)) {
+  console.log(keys, ":", clothingItem[key]);
+}
+
+function testBracketsDynamicAccess() {
+  var dynamicKey = Math.random() > 0.5 ? "speed" : "color";
+
+  var drone = {
+    speed: 15,
+    color: "orange",
+  };
+
+  console.log(drone[dynamicKey]);
+}
+testBracketsDynamicAccess();
